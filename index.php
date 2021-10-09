2ee85bd382a8b5d5ca75cf77c6251ebd64e4961f
kemaltf <kemaltaufikf@gmail.com>
2021-10-09 17:39:51 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Coba git remote</title>
 </head>
 <body>
-    <h1>Hello world    </h1>
+    <h1>Hello world     </h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptatum incidunt earum dolor, consequatur blanditiis debitis aperiam impedit voluptatibus minus sint mollitia doloremque similique sit vel recusandae nam adipisci atque.</p>
 </body>
 </html>
